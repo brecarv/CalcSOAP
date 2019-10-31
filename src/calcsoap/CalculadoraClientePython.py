80e216badc71d75fa9599f6e31d864ce5d58b56f
Emerson Carvalho <brecarv@outlook.com>
2019-10-30 23:23:08 -0300

    soma

--- src/calcsoap/CalculadoraClientePython.py
+++ src/calcsoap/CalculadoraClientePython.py
@@ -2,4 +2,11 @@ import zeep
 
 wsdl = 'http://127.0.0.1:9000/calcsoap?wsdl'
 client = zeep.Client(wsdl=wsdl)
+
+print("Soma: ")
 print(client.service.soma(5, 2))
+print(client.service.soma(2, 2))
+print(client.service.soma(40, 15))
+
+
+
